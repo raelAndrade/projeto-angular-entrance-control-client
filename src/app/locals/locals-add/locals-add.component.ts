4fc66d290c198bcb533699f8778879a15d44bb63
raelAndrade <israelg.andrade@bol.com.br>
2020-01-23 17:01:49 -0300

    create autocomplete

--- src/app/locals/locals-add/locals-add.component.ts
+++ src/app/locals/locals-add/locals-add.component.ts
@@ -10,9 +10,28 @@ import { AdmService } from '../adm.service';
 })
 export class LocalsAddComponent implements OnInit {
 
-  adm: Administrations[] = [];
+  admin: any[];
+  filteredAdminSingle: any[];
 
   constructor(private service: AdmService) { }
+
+  filterAdminSingle(event) {
+    let query = event.query;
+    this.service.list().then(admin => {
+      this.filteredAdminSingle = this.filterAdmin(query, admin);
+    });
+  }
+
+  filterAdmin(query, admin: any[]): any[] {
+    let filtered: any[] = [];
+    for (let i = 0; i < admin.length; i++) {
+      let adm = admin[i];
+      if (adm.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(adm);
+      }
+    }
+    return filtered;
+  }
 
   ngOnInit() {
     this.service.list().subscribe(dados => this.adm = dados);
